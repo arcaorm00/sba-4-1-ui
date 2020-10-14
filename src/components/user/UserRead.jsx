c3d20ec15423a64e908e416af0427ef6d5a5ee60
salt <arcaorm00@gmail.com>
2020-10-14 15:28:17 +0900

    15:28

--- src/components/user/UserRead.jsx
+++ src/components/user/UserRead.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import axios from 'axios'
 
 const UserRead = () => {
     const [userid, setUserid] = useState()
@@ -8,10 +9,19 @@ const UserRead = () => {
         e.preventDefault()
         // alert(`로그인 버튼 클릭 ${document.getElementById('id').value}`)
         alert(`로그인 아이디: ${userid}, 비밀번호: ${userpwd}`)
+        axios.post('', {userid, userpwd})
+        .then(res => {
+            alert('Success')
+        })
+        .catch(error => {
+            alert('Fail')
+        })
+        //post(보냈더니).then(성공한 경우).catch(실패한 경우)
     }
 
-    const cancel = () => {
-        alert('cancel button click')
+    const cancel = e => {
+        e.preventDefault()
+
     }
 
     return (<>
@@ -23,7 +33,7 @@ const UserRead = () => {
         </tr>
         <tr>
             <td>Password</td>
-            <td><input type='text' id='pwd' name='pwd' onChange={e => {setUserPwd(`${e.target.value}`)}}/></td>
+            <td><input type='text' id='pwd' onChange={e => {setUserPwd(`${e.target.value}`)}}/></td>
         </tr>
         <tr>
             <td colSpan='2'>
